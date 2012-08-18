9e574f5406ecc8d70efc0ad4e75e4828a4d211b3
Alex Krasinsky <lyoshakr@gmail.com>
2012-08-18 17:15:53 +0300

    require_dependency fixed

--- init.rb
+++ init.rb
@@ -1,4 +1,5 @@
 require 'redmine'
+require 'rails'
 
 # Taken from lib/redmine.rb
 if RUBY_VERSION < '1.9'
@@ -7,16 +8,18 @@ else
   require 'csv'
 end
 
-Rails.application.config.after_initialize do
-  # TODO: require_dependency and to_prepare callback will trigger an error in association, find a solution
-  # This way it will be bugged in development
-  User.send(:include, TimesheetPlugin::Patches::UserPatch)
-  Project.send(:include, TimesheetPlugin::Patches::ProjectPatch)
-  # Needed for the compatibility check
-  begin
-    require_dependency 'time_entry_activity'
-  rescue LoadError
-    # TimeEntryActivity is not available
+class TimesheetRailtie < Rails::Railtie
+  config.to_prepare do
+    require_dependency 'project'
+    require_dependency 'principal'
+    require_dependency 'user'
+    User.send(:include, TimesheetPlugin::Patches::UserPatch)
+    Project.send(:include, TimesheetPlugin::Patches::ProjectPatch)
+    begin
+      require_dependency 'time_entry_activity'
+    rescue LoadError
+      # TimeEntryActivity is not available
+    end
   end
 end
 
@@ -31,22 +34,22 @@ Redmine::Plugin.register :timesheet do
   requires_redmine :version_or_higher => '0.9.0'
 
   settings(:default => {
-             'list_size' => '5',
-             'precision' => '2',
-             'project_status' => 'active',
-             'user_status' => 'active'
-           }, :partial => 'settings/timesheet_settings')
+      'list_size'      => '5',
+      'precision'      => '2',
+      'project_status' => 'active',
+      'user_status'    => 'active'
+  },       :partial => 'settings/timesheet_settings')
 
   permission :see_project_timesheets, { }, :require => :member
 
   menu(:top_menu,
        :timesheet,
-       {:controller => 'timesheet', :action => 'index'},
+       { :controller => 'timesheet', :action => 'index' },
        :caption => :timesheet_title,
-       :if => Proc.new {
+       :if      => Proc.new {
          User.current.allowed_to?(:see_project_timesheets, nil, :global => true) ||
-         User.current.allowed_to?(:view_time_entries, nil, :global => true) ||
-         User.current.admin?
+             User.current.allowed_to?(:view_time_entries, nil, :global => true) ||
+             User.current.admin?
        })
 
 end
